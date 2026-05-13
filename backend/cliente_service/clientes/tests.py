0692f32d5d896a133a30a98403c4f454bab48c6a
Antonio58306 <a58306@alunos.ipb.pt>
2026-05-14 00:07:45 +0100

    Implementação de testes unitários

--- backend/cliente_service/clientes/tests.py
+++ backend/cliente_service/clientes/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Cliente
+import uuid
 
-# Create your tests here.
+class ClienteAPITestCase(TestCase):
+    def setUp(self):
+        self.client_api = APIClient()
+        self.client = Cliente.objects.create(
+            nif='123456789',
+            nome='João Silva',
+            email='joao@email.com'
+        )
+    def tes_listar_clientes(self):
+        response = self.client_api.get('/api/clientes/')
+        self.assertEqual(response.status_code, 200)
