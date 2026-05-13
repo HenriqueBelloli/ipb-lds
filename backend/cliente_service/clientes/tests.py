c95d1bb2359aecc37c393a57b2e8d704764ef814
Antonio58306 <a58306@alunos.ipb.pt>
2026-05-14 00:28:38 +0100

    Configuração de serviço de criar, atualizar e deletar cliente

--- backend/cliente_service/clientes/tests.py
+++ backend/cliente_service/clientes/tests.py
@@ -14,3 +14,29 @@ class ClienteAPITestCase(TestCase):
     def tes_listar_clientes(self):
         response = self.client_api.get('/api/clientes/')
         self.assertEqual(response.status_code, 200)
+
+    
+    def test_criar_cliente(self):
+        response = self.client_api.post('/api/clientes/', {
+            'nif': '987654321',
+            'nome': 'Maria Santos',
+            'email': 'maria@email.com'
+        }, format='json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_detalhe_cliente(self):
+        response = self.client_api.get(f'/api/clientes/{self.client.id}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_atualizar_cliente(self):
+        response = self.client_api.patch(f'/api/clientes/{self.cliente.id}/', {
+            'telefone':'912345678'
+        }, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_eliminar_cliente(self):
+        response = self.client_api.delete(f'/api/clientes/{self.cliente.id}/')
+        self.assertEqual(response.status_code, 204)
+
+    
+
