a5e6ebbc621b4f6547f83b086c23cc309c5be482
Antonio58306 <a58306@alunos.ipb.pt>
2026-05-13 23:51:42 +0100

    Implementação do serviço de gestão de clientes

--- backend/cliente_service/clientes/admin.py
+++ backend/cliente_service/clientes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Cliente, ClienteDelegacao
 
-# Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'nif', 'email', 'ativo', 'flagAssociado']
+    search_fields = ['nome', 'nif', 'email']
+    list_filter = ['ativo', 'flagAssociado']
+
+
+@admin.register(ClienteDelegacao)
+class ClienteDelegacaoAdmin(admin.ModelAdmin):
+    list_display = ['clienteId', 'delegacaoId', 'createAt']
