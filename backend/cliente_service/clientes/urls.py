f295a389b34ce3d63bdee89b7aab366e32d3aa4e
Antonio58306 <a58306@alunos.ipb.pt>
2026-05-19 08:58:41 +0100

    Configurando as rotas

--- backend/cliente_service/clientes/urls.py
+++ backend/cliente_service/clientes/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 from .views import ClienteViewSet
 
-router = DefaultRouter()
-router.register(r'clientes', ClienteViewSet, basename='cliente')
+cliente_list = ClienteViewSet.as_view({'get':'list', 'post': 'create',})
+cliente_detail = ClienteViewSet.as_view({'get': 'retrieve', 'put':'update'})
+cliente_delegs = ClienteViewSet.as_view({'get': 'delegacoes', 'post': 'delegacoes'})
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('clientes/', cliente_list),
+    path('clientes/<uuid:pk>/', cliente_detail),
+    path('clientes/<uuid:pk>/delegacoes/', cliente_delegs),
+]
