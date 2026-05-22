da6b2ecc019f8f886a55d82a54f688a344df57b1
Antonio58306 <a58306@alunos.ipb.pt>
2026-05-22 16:40:23 +0100

    Criaçao de modelos que vão definir as tabelas Serviço e ServiçoDelegacao.

--- backend/servico-service/core/models.py
+++ backend/servico-service/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+import uuid
+from .validators import validar_percentual_entrada
 
-# Create your models here.
+class Servico(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True, null=True)
+    flagBonificavel = models.BooleanField(default=False)
+    ativo = models.BooleanField(default=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+class ServicoDelegacao(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    servicoId = models.ForeignKey(Servico, on_delete=models.CASCADE, related_name='delegacoes')
+    delegacaoId = models.UUIDField() # Referencia externa (apenas o UUID, não gera FK física)
+    precoAssociado = models.DecimalField(max_digits=10, decimal_places=2)
+    precoNaoAssociado = models.DecimalField(max_digits=10, decimal_places=2)
+    percentualEntrada = models.DecimalField(max_digits=5,
+                                            decimal_places=2,
+                                            default=0,
+                                            validators=[validar_percentual_entrada])
+    ativo = models.BooleanField(default=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ['servicoId', 'delegacaoId']
+
+    def __str__(self):
+        return f'{self.servicoId.nome} - Delegação {self.delegacaoId}'
+
