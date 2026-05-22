8224dcaba4c112a514bf7b2a0cae3cd1ca219379
Pedro Henrique Teodoro de Mendonca <a69578@alunos.ipb.pt>
2026-05-22 11:42:26 +0100

    Arrumando as envs do settings.py

--- backend/usuario_service/app/core/settings.py
+++ backend/usuario_service/app/core/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
