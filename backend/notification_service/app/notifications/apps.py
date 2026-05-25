d5c1bc9df072ba05df5d3ed6b5a76dbab3441878
Pedro Henrique Teodoro de Mendonca <a69578@alunos.ipb.pt>
2026-05-25 16:28:47 +0100

    Implementacao dos consumers e adicao da colecao do postman (ainda sem autenticacao)

--- backend/notification_service/app/notifications/apps.py
+++ backend/notification_service/app/notifications/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class NotificationsConfig(AppConfig):
     name = 'notifications'
+
+    def ready(self):
+        import sys
+        if "migrate" in sys.argv:
+            return
+        
+        from .consumers import iniciar_consumers
+        iniciar_consumers()
---
+++ backend/notification_service/app/notifications/services/cliente_client.py
@@ -0,0 +1,32 @@
+import os
+import requests
+
+class ClienteServiceClient:
+    BASE_URL = os.environ.get('CLIENTE_SERVICE_URL', 'http://cliente-service:8003')
+
+    @staticmethod
+    def get_cliente_anonimo(cliente_id : str) -> dict:
+        """
+        Chamada sem token de utilizador - usa token de serviço interno.
+        Retorna apenas eamil e nome do cliente.
+        Em caso de falha retorna None sem derrubar o serviço
+        """
+
+        try:
+            response = requests.get(
+                f"{ClienteServiceClient.BASE_URL}",
+                f"/api/clientes/{cliente_id}/",
+                headers= {
+                    "Authorization": f"Bearer {os.environ.get('INTERNAL_SERVICE_TOKEN')}"
+
+                },
+                timeout=5
+            )
+
+            if response.status_code == 200:
+                return response.json()
+            
+            return None
+        
+        except requests.exceptions.RequestException:
+            return None
